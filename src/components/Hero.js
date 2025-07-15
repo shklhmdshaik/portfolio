5b207a6905b32de16c82c09d354d11483af07c55
Shakeel Ahmed <shklhmdshaik@gmail.com>
2025-07-15 21:03:19 +0400

    Your update message here

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import shakeel from "../assets/images/shakeel.jpg"
 function Hero() {
   return (
-    <section id="home" className="container text-white p-3 d-flex  max-w-90 shadow my-5">
-      <div className='w-25'>
-        <img src={shakeel} alt="Shakeel" className='img-fluid rounded-circle rotate-hover p-5' width="max-width: 150px" height="auto"/>
+    <section id="home" className="container text-white py-5 my-5 shadow mx-auto max-w-90">
+      <div className='row align-items-center'>
+        <div className='col-md-4 text-center'>
+        <img src={shakeel} alt="Shakeel" className='img-fluid rounded-circle rotate-hover ps-2 text-center text-lg-start' style={{maxWidth: "200px", height:"auto"}}/>
       </div>
-      <div className='w-75'>
-        <h1 className="display-4 py-5 fw-bold">Shaik Shakeel Ahmed</h1>
+      
+      <div className='col-md-8 text-center text-lg-start'>
+        <h1 className="display-5 pt5-5 pb-3 fw-bold">Shaik Shakeel Ahmed</h1>
         <p className="lead fw-semibold">React js Developer | Front End Developer | UI Developer</p>
       </div>
 
-
+</div>
 
     </section>
   );
