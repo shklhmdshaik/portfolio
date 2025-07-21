95d7bb06b978c0f1e89837829c1b8b1f796525ce
Shakeel Ahmed <shklhmdshaik@gmail.com>
2025-07-21 16:07:55 +0400

    Header and footer alignments updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import { Container } from 'react-bootstrap';
 
 function Footer() {
   return (
-    <footer className="text-white text-center py-3 my-5 max-w-90 mx-auto">
-      <Container>
+    <footer className="text-white py-3 mt-5 mx-auto">
+      <Container fluid>
         <small>© {new Date().getFullYear()} Shakeel Ahmed. All rights reserved.</small>
       </Container>
     </footer>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function PortfolioNavbar() {
   return (
-    <Navbar variant="dark" expand="lg" fixed="top" className='nav-container mb-1 position-sticky mt-2 mx-auto max-w-90'>
-      <Container>
+    <Navbar variant="dark" expand="lg" fixed="top" className='nav-container mb-1 position-sticky mt-2 mx-auto'>
+      <Container fluid>
         <Navbar.Brand href="#home">Shakeel Ahmed</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
