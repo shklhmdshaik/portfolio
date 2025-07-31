374d83d01e0b67e184ce7a68ab74edc47e517a5b
Shakeel Ahmed <shklhmdshaik@gmail.com>
2025-07-31 11:35:23 +0400

    hamburger menu updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function PortfolioNavbar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClick = () => {
+    setExpanded(false); // close menu when any link is clicked
+  };
   return (
-    <Navbar variant="dark" expand="lg" fixed="top" className='nav-container mb-1 position-sticky mt-2 mx-auto'>
+    <Navbar expanded={expanded} variant="dark" expand="lg" fixed="top" className='nav-container mb-1 position-sticky mt-2 mx-auto'>
       <Container fluid>
         <Navbar.Brand href="#home">Shakeel Ahmed</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(prev => !prev)} />
+        <Navbar.Collapse id="basic-navbar-nav" >
+            <Nav className="ms-auto">
+              <Nav.Link href="#about" onClick={handleNavClick}>About</Nav.Link>
+              <Nav.Link href="#skills" onClick={handleNavClick}>Skills</Nav.Link>
+              <Nav.Link href="#projects" onClick={handleNavClick}>Projects</Nav.Link>
+              <Nav.Link href="#contact" onClick={handleNavClick}>Contact</Nav.Link>
+            </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
